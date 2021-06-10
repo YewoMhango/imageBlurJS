56c83bef4c269ea8e2a79e3b03315348a7ed97a4
YewoMhango <mhangoyewoh@gmail.com>
2021-06-10 14:20:37 +0200

    Updated faux Gaussian blur to use multiplied box blur due that having been fixed

--- algorithms/gaussianBlur.js
+++ algorithms/gaussianBlur.js
@@ -1,10 +1,6 @@
 // This computes a false Gaussian blur using three passes of a 
 // box blur. For more information check out the link below:
 //  - http://blog.ivank.net/fastest-gaussian-blur.html
-// I ended up choosing `furtherOptimizedBoxBlur` over `multipliedBoxBlur`
-// since the distortion of `multipliedBoxBlur` tend
-// to get exacerbated when it gets used in multiple passes
-// on an image
 
 /**
  * Computes a Gaussian blur using three passes of a box blur 
@@ -18,9 +14,9 @@
  * @returns {RGBImage} A blurred image
  */
 function fauxGaussianBlur(image, band, radius) {
-  return furtherOptimizedBoxBlur(
-    furtherOptimizedBoxBlur(
-      furtherOptimizedBoxBlur(image, band, radius),
+  return multipliedBoxBlur(
+    multipliedBoxBlur(
+      multipliedBoxBlur(image, band, radius),
       band,
       radius
     ),
